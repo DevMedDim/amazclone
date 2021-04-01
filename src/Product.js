cbda0894fada606b165c21ef7a6600176f983c4b
DevMedDim <mrakdimdev@gmail.com>
2021-04-01 22:53:01 +0100

    fixed the shopping cart

--- src/Product.js
+++ src/Product.js
@@ -1,23 +1,40 @@
 import React from 'react'
 import './Product.css'
+import { useStateValue } from './StateProvider';
 
-function Product({id,title,price,rating,image}) {
+function Product({ id, title, price, rating, image }) {
+    const [{basket}, dispatch] = useStateValue();
+    console.log("this is the basket => ", basket);
+    
+    const addToBasket = () => {
+        //dispatch the item into the data layer
+        dispatch({
+            type: "ADD_TO_BASKET",
+            item: {
+                id: id,
+                title: title,
+                price: price,
+                rating: rating,
+                image:image,
+            },
+        });
+    };
     return (
         <div className="product">
             <div className="product__info">
                 <p> {title} </p>
                 <p className="product__price">
-                    <small>DH</small>
-                    <strong> {price} </strong>
+                    <small>$</small>
+                    <strong  > {price} </strong>
                 </p>
-                <div className="product__rating">
-                    {Array(rating).fill().map((_,i)=>(
-                    <p>⭐</p>    
+                <div   className="product__rating">
+                    {Array(rating).fill().map((_, i) => (
+                        <p key={i} >⭐</p>
                     ))}
                 </div>
             </div>
-            <img src={image} alt=""/>
-            <button>Add to Basket</button>
+            <img   src={image} alt="" />
+            <button onClick={addToBasket} >Add to Basket</button>
         </div>
     )
 }
