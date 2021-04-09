cbd4ecd2a1bded0986d112f3ee0a9eff63005530
DevMedDim <mrakdimdev@gmail.com>
2021-04-09 23:27:46 +0100

    user authentication state change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import './App.css'
 import Header from './Header'
@@ -6,35 +6,61 @@ import Home from './Home'
 import Checkout from './Checkout'
 import PageNotFound from './PageNotFound'
 import Login from "./Login"
+import { auth } from "./firebase"
+import { useStateValue } from "./StateProvider"
 
 
 function App() {
-  return (
-    //BEM convention
-    <Router>
-      <div className="app">
-        <Switch>
-          <Route path='/login'>
-            <Login></Login>
-          </Route>
-          <Route exact path='/'>
-            <Header></Header>
-            <Home></Home>
-          </Route>
+  const [{},dispatch] = useStateValue();
+  useEffect(() => {
+    // runs only once, when the app component loads... 
+    auth.onAuthStateChanged(authUser => {
+      console.log('THE USER IS :', authUser);
+      if (authUser){
+        // the user just logged in / the user was logged in
+      
+dispatch({
+  type: 'SET_USER',
+  user: authUser
+})
+      }
+      else{
+        // the user is logged out
+        dispatch({
+          type:'SET_USER',
+          user:null
+        })
+      }
+    })
+    
+  },[dispatch])
 
-          <Route path='/checkout'>
-            <Header></Header>
-            <Checkout></Checkout>
-          </Route>
+    return (
+      //BEM convention
+      <Router>
+        <div className="app">
+          <Switch>
+            <Route path='/login'>
+              <Login></Login>
+            </Route>
+            <Route exact path='/'>
+              <Header></Header>
+              <Home></Home>
+            </Route>
 
-          <Route component={PageNotFound} />
+            <Route path='/checkout'>
+              <Header></Header>
+              <Checkout></Checkout>
+            </Route>
+
+            <Route component={PageNotFound} />
 
 
-        </Switch>
+          </Switch>
 
-      </div>
-    </Router>
-  );
-}
+        </div>
+      </Router>
+    );
+  }
 
 export default App;
